1063789a60518a3e592e45ea5a6ea00603734b18
Jeremy <jeremy.lorelli.1337@gmail.com>
2019-03-25 19:43:23 -0700

    Update vpc scripts to support win64

--- src/createeverything.bat
+++ src/createeverything.bat
@@ -1,3 +1,4 @@
 @echo off
 devtools\bin\vpc.exe /hl2r /hl2 /hl2mp /episodic +everything /mksln everything.sln
+devtools\bin\vpc.exe /hl2r /hl2 /hl2mp /episodic +everything /mksln everything_x64.sln /win64
 pause
