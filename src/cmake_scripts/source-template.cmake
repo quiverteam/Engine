f8e398d64964bd61abe615a0fb2a0dc6fe1dfbf8
Jeremy <jeremy.lorelli.1337@gmail.com>
2019-06-10 13:21:14 -0700

    Update the template file

--- src/cmake_scripts/source-template.cmake
+++ src/cmake_scripts/source-template.cmake
@@ -6,26 +6,37 @@
 project(${PROJECT} C CXX)
 
 # Platform specific sources 
-set(WIN32_SRCS	)
+set(WINDOWS_SRCS	)
 set(POSIX_SRCS	)
 
 # Generic sources
 set(SRCS	)
 
-# Directories where all source files will be taken from
-set(EXTRA_SRC_DIRS	)
-
 # Preprocessor defs
 set(DEFINES	)
 
 # Platform-specific preprocessor defines
-set(WIN32_DEFINES	)
+set(WIN32_DEFINES	)	
+set(POSIX32_DEFINES	)
+set(WIN64_DEFINES	)
+set(POSIX64_DEFINES	)
+set(WINDOWS_DEFINES	)
 set(POSIX_DEFINES	)
+
 set(PLATFORM_64BIT_DEFINES	)
 set(PLATFORM_32BIT_DEFINES 	)
 
 # Generic libs to link against
 set(LINK_LIBS	)
+
+# Links dirs to search for link libs in
+set(LINK_DIRS	)
+set(WINDOWS_LINK_DIRS	)
+set(POSIX_LINK_DIRS		)
+set(WIN32_LINK_DIRS		)
+set(WIN64_LINK_DIRS		)
+set(POSIX32_LINK_DIRS	)
+set(POSIX64_LINK_DIRS	)
 
 # Platform specific link libs
 set(WIN32_LINK_LIBS		)
@@ -47,6 +58,12 @@ set(TARGET ${PROJECT})
 
 # Set the output file name
 set(OUTPUT_FILE_NAME ${PROJECT})
+
+# If this is a shared lib
+set(SHARED_LIB 1)
+
+# If this is a static lib
+set(STATIC_LIB 0)
 
 # Include this to handle all the defines
 include(${CMAKESCRIPTS_DIR}/library-base.cmake)
