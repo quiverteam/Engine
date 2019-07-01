e54a43a0a08cc82fe916579dee127800a7b2aeac
Demez <demizexp@gmail.com>
2019-07-01 14:55:54 -0400

    changed _kill_shadercompiler.bat echo

--- src/materialsystem/stdshaders/_kill_shadercompiler.bat
+++ src/materialsystem/stdshaders/_kill_shadercompiler.bat
@@ -3,6 +3,6 @@
 tasklist /FI "IMAGENAME eq shadercompile.exe" 2>NUL | find /I /N "shadercompile.exe">NUL
 
 if "%ERRORLEVEL%"=="0" (
-	echo Shadercompile is running still, killing
+	echo shadercompile.exe is still running, killing
 	taskkill /f /im shadercompile.exe >nul
 )
