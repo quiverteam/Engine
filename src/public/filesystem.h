295fe4ac962c6991ca5ca077555d9e019d3ca2c7
jjl772 <jeremy.lorelli.1337@gmail.com>
2019-07-29 21:28:57 -0700

    [Filesystem] Change signature of FPrintf to avoid warnings.

--- src/public/filesystem.h
+++ src/public/filesystem.h
@@ -1,4 +1,4 @@
-//===== Copyright © 1996-2005, Valve Corporation, All rights reserved. ======//
+//===== Copyright ï¿½ 1996-2005, Valve Corporation, All rights reserved. ======//
 //
 // Purpose: 
 //
@@ -484,7 +484,7 @@ public:
 	virtual bool			EndOfFile( FileHandle_t file ) = 0;
 
 	virtual char			*ReadLine( char *pOutput, int maxChars, FileHandle_t file ) = 0;
-	virtual int				FPrintf( FileHandle_t file, char *pFormat, ... ) = 0;
+	virtual int				FPrintf( FileHandle_t file, const char *pFormat, ... ) = 0;
 
 	//--------------------------------------------------------
 	// Dynamic library operations
