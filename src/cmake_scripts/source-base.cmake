b88d69f685f59b5d9eeb252d90378114656022ca
jjl772 <jeremy.lorelli.1337@gmail.com>
2019-10-26 13:02:39 -0700

    Update to CMake stuff, does not work because git trashed my unstaged changes.
    Git can go fuck itself.

--- src/cmake_scripts/source-base.cmake
+++ src/cmake_scripts/source-base.cmake
@@ -8,16 +8,14 @@ set(CMAKE_CXX_STANDARD 11)
 
 if(DEFINED POSIX OR CLANG_BUILD)
 	if(NOT BUILD_64BIT)
-		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags")
-		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags")
+		set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -m32" CACHE STRING "c++ flags" FORCE)
+		set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -m32" CACHE STRING "c flags" FORCE)
 		set(CMAKE_LINK_FLAGS "${CMAKE_LINK_FLAGS} -m32")
 	endif(NOT BUILD_64BIT)
 	
-	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ffast-math -mtune=core2 -msse3")
-	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffast-math -mtune=core2 -msse3")
+	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ffast-math -march=i686 -msse3")
+	set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -ffast-math -march=i686 -msse3")
 endif()
-
-
 
 if(DEFINED POSIX)
 	set(CMAKE_LINK_FLAGS "${CMAKE_LINK_FLAGS} -lc")
