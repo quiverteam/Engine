c0632a921628e6f9c6f11bde086b10cfbacc143c
jjl772 <jeremy.lorelli.1337@gmail.com>
2019-10-26 13:01:33 -0700

    Get rid of old-style assertions and replace with static_assert

--- src/public/tier0/dbg.h
+++ src/public/tier0/dbg.h
@@ -566,20 +566,7 @@ public:
 // We're using an ancient version of GCC that can't quite handle some
 // of our complicated templates properly.  Use some preprocessor trickery
 // to workaround this
-#ifdef __GNUC__
-	#define COMPILE_TIME_ASSERT( pred ) typedef int UNIQUE_ID[ (pred) ? 1 : -1 ]
-#else
-	#if _MSC_VER >= 1600
-	// If available use static_assert instead of weird language tricks. This
-	// leads to much more readable messages when compile time assert constraints
-	// are violated.
-	#define COMPILE_TIME_ASSERT( pred ) static_assert( pred, "Compile time assert constraint is not true: " #pred )
-	#else
-	// Due to gcc bugs this can in rare cases (some template functions) cause redeclaration
-	// errors when used multiple times in one scope. Fix by adding extra scoping.
-	#define COMPILE_TIME_ASSERT( pred ) typedef char compile_time_assert_type[(pred) ? 1 : -1];
-	#endif
-#endif
+#define COMPILE_TIME_ASSERT(pred) static_assert(pred, "Compile time assertion failed: " #pred)
 // ASSERT_INVARIANT used to be needed in order to allow COMPILE_TIME_ASSERTs at global
 // scope. However the new COMPILE_TIME_ASSERT macro supports that by default.
 #define ASSERT_INVARIANT( pred )	COMPILE_TIME_ASSERT( pred )
