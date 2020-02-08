e93f273746c9de75a0ecd0bd4fcee0d222e0e846
Demez <demizexp@gmail.com>
2020-02-08 18:00:47 -0500

    [build system] rename pyvpc scripts, keeping for now just in case if needed again

--- src/_pyvpc_coreprojects.bat
+++ src/_pyvpc_coreprojects.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set pyqpc=py devtools\pyqpc\PyQPC.py /basefile "../../vpc_scripts/default.vgc"
-%pyqpc% /hl2 /hl2mp /episodic +libraries /vs2019 /mksln libraries
+%pyqpc% /hl2 /hl2mp /episodic +core /vs2019 /mksln pyvpc_core
 
 pause
--- src/_pyvpc_libprojects.bat
+++ src/_pyvpc_libprojects.bat
@@ -1,6 +1,6 @@
 @echo off
 
 set pyqpc=py devtools\pyqpc\PyQPC.py /basefile "../../vpc_scripts/default.vgc"
-%pyqpc% /hl2 /hl2mp /episodic +everything /vs2019 /mksln everything
+%pyqpc% /hl2 /hl2mp /episodic +libraries /vs2019 /mksln pyvpc_libraries
 
 pause
