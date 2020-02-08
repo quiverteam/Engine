914da482a856ad14de4202c7abc00b2c276cc6f7
Demez <demizexp@gmail.com>
2020-02-07 19:33:24 -0500

    [qpc] add batch scripts for qpc

---
+++ src/_create_engine.bat
@@ -0,0 +1,5 @@
+@echo off
+
+devtools\qpc\qpc.py -a game -t vstudio -m hl2 hl2mp episodic -mf engine
+
+pause
---
+++ src/_create_tools.bat
@@ -0,0 +1,5 @@
+@echo off
+
+devtools\qpc\qpc.py -b "_qpc_scripts/_default.qpc_base" -a utils -t vstudio -mf utils
+
+pause
