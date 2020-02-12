cecc34dafdbaec5c5db64c33f001029882e42646
Demez <demizexp@gmail.com>
2020-02-11 21:13:27 -0500

    [game] add batch files to run episodic and hl2mp

--- game/run_mod_episodic.bat
+++ game/run_mod_episodic.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set cmd_line=-console -sw -noborder -game mod_hl2
+set cmd_line=-console -sw -noborder -game mod_episodic
 
 start "" bin\win32\quiver.exe %cmd_line%
 echo bin\win32\quiver.exe %cmd_line%
--- game/run_mod_hl2mp.bat
+++ game/run_mod_hl2mp.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set cmd_line=-console -sw -noborder -game mod_hl2
+set cmd_line=-console -sw -noborder -game mod_hl2mp
 
 start "" bin\win32\quiver.exe %cmd_line%
 echo bin\win32\quiver.exe %cmd_line%
