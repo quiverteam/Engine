316e359898de28fea71ec7040f6f87c8047337c5
Demez <demizexp@gmail.com>
2020-04-18 12:30:54 -0400

    [pipelines/qpc] don't build physics for now

--- src/_create_engine.bat
+++ src/_create_engine.bat
@@ -1,5 +1,5 @@
 @echo off
 
-py devtools\qpc\qpc.py -b "_qpc_scripts/_default.qpc_base" -a game -g visual_studio -m HL2 HL2MP EPISODIC -mf engine
+py devtools\qpc\qpc.py -b "_qpc_scripts/_default.qpc_base" -a game -r physics -g visual_studio -m HL2 HL2MP EPISODIC -mf engine
 
 pause
