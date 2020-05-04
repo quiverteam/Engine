bcfb8a7e0c0ed9910689d00eba661a2fc7f19205
Demez <demizexp@gmail.com>
2020-05-04 18:42:47 -0400

    [general] remove pyvpc batch scripts, pyvpc was removed a bit ago

--- src/_pyvpc_engineandtools.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-set pyqpc=py devtools\pyqpc\PyQPC.py /basefile "../../vpc_scripts/default.vgc"
-set "disabled_projects=-bsppack -bzip2 -ep2_deathmap -vtex_dll -vtex_launcher -normal2ssbump -height2normal -height2ssbump"
-%pyqpc% /hl2 /hl2mp /episodic +game +utils +tools %disabled_projects% /vs2019 /mksln pyvpc_engine_and_tools
-
-pause
